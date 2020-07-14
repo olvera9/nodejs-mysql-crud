bc863a47081d02c68304c819919ba54f7281afac
olvera9 <olvera9@gmail.com>
2020-07-14 14:42:12 -0500

    Updated subdomain to ea.olvera9.com

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=("olvera9.com" "www.olvera9.com")
+domains=("ea.olvera9.com")
 rsa_key_size=4096
 data_path="./data/certbot"
 email="joaquin.olvera@softtek.com" # Adding a valid address is strongly recommended
